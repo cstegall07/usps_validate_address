5c0a6781db2be94e50fd9f895b52553a62ac4c86
cstegall07 <charmainestegall@Charmaines-MacBook-Air.local>
2022-08-18 13:47:44 -0400

    updating files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,94 @@
-import React from 'react' ;
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const intialAddress = {address1: "", address2: "", city: "", state: ""};
+  const [zipcode, setZipcode] = useState("")
+  const [address, setAddress] = useState(intialAddress)
+
+  useEffect(() => {
+
+    const fetchAddress = async () => {
+      try {
+        const response = await fetch(
+          `/.netlify/functions/getAddress?address1=${address.address1}&address2=${address.address2}&city=${address.city}&state=${address.state}zipcode=${zipcode}`,
+          { headers: { accept: "application/json" } }
+        );
+        const data = await response.text();
+        console.log(data)
+
+        setAddress({...address, address1:"data", address2:"", city:"", state:""})
+  
+      
+    } catch (e){
+      console.log(e)
+    }
+  }; fetchAddress();
+  })
   return (
     <div className="App">
-      
+      <h1>Goodr Verify Address</h1>
+      <form action="" className="form-data">
+        <label htmlFor="addr1">Address 1</label>
+        <input
+          className="addr1"
+          placeholder=""
+          type="text"
+          name="addr1"
+          id="addr1"
+          />
+        <label htmlFor="addr2">Address 2</label>
+        <input
+          className="addr2"
+          placeholder=""
+          type="text"
+          name="addr2"
+          id="addr2"
+          />
+        <label htmlFor="addrcity">City</label>
+        <input
+          className="addrcity"
+          placeholder=""
+          type="text"
+          name="addrcity"
+          id="addrcity"
+          />
+        <label htmlFor="addrstate">State</label>
+        <input
+          className="addrstate"
+          placeholder=""
+          type="text"
+          name="addrstate"
+          id="addrstate"
+          />
+        <label htmlFor="addrzip5">Zip Code</label>
+        <input
+          className="addrzip5"
+          value={zipcode || ""}
+          placeholder="XXXXX"
+          type="text"
+          name="addrzip5"
+          id="addrzip5"
+          onChange={(event) => {
+            const { value } = event.target;
+            setZipcode(value.replace(/[^\d{5}]$/, "").substr(0, 5));
+          }}
+          />
+      </form>
+      {/* <pre>
+        <code>
+          {JSON.stringify({
+            addr1: address.address1,
+            addr2: address.address2,
+            city: address.city,
+            state: address.state,
+            zip: address.zip5
+            
+          })}
+        </code>
+      </pre> */}
     </div>
   );
 }
 
-export default App;
+export default App
